45dba8293c1d66c9e60f56ef94b8789d202d74e6
felipe.baeta <baetafelipe2@gmail.com>
2021-09-05 21:05:50 -0300

    case 1 - ok

--- case1/wp-config.php
+++ case1/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dev' );
+define( 'DB_NAME', 'webdev_case1' );
 
 /** MySQL database username */
 define( 'DB_USER', 'webdev_case1' );
